2a8e0d8c20ae7167c7c937cbe2329e44cb881b0e
shrishti-pillay <shrishti98pillay@gmail.com>
2025-06-03 15:13:31 +0800

    feat: Add css styling and update response structure

--- breed_finder/home/static/search.js
+++ breed_finder/home/static/search.js
@@ -1,6 +1,16 @@
 
- const search = async () => {
+ document.querySelector('#search').addEventListener('click',async (e) => {
+    e.preventDefault(); // Prevent the default form submission behavior
     const searchInput = document.querySelector("#prompt")
+    console.log("Search input value:", searchInput.value);
+
+    // show loading animation // hide previous results if any
+    const loader_container = document.querySelector("#loader-container").style;
+    const results_container = document.querySelector("#results-container");
+    results_container.style.display = "none";
+    loader_container.display = "block";
+
+    // fetch url response
     const search_url = "/home/search?prompt="+ searchInput.value;
     await fetch(search_url, {
         method: "GET",
@@ -15,13 +25,20 @@
       return response.json()
     })
     .then(data => {
-        // Process the data
-        const results_container = document.querySelector("#results-container");
-        const img_element = document.createElement("img");
+        
+        loader_container.display = "none"; // Hide the loader
+        // Display the results container
+        results_container.style.display = "block"; 
+
+        // Set the image source and title
+        const title_element = results_container.querySelector("#title");
+        title_element.innerHTML=  `${data['breed']} `;
+        const img_element = results_container.querySelector("#result-image");
         img_element.src = data['img_url'];
-        results_container.appendChild(img_element);
+        const image_container = results_container.querySelector("#image-container");
+        image_container.appendChild(img_element);
     })
     .catch(error => {
         console.error('There was a problem with the fetch operation:', error);
     });
-};
+});
--- breed_finder/home/views.py
+++ breed_finder/home/views.py
@@ -14,12 +14,15 @@ def search(request):
         if request.method == 'GET':
             query = request.GET.get('prompt', '')
             results = query_results("text",query)
+            print(results)
             if len(results) >= 1:
                 best_result = results[0]
-                
-                img_url = get_presigned_url('breed-finder-v1',"dog7.jpg", 3600 )
+                img_id = best_result[0]
+                metadata = best_result[1]
+                img_url = get_presigned_url('breed-finder-v1',img_id, 3600 )
                 data = {
-                    "img_url": img_url
+                    "img_url": img_url,
+                    "breed": metadata.get('breed', '')
                 }
                 return HttpResponse(json.dumps(data),content_type="application/json")
             else:
